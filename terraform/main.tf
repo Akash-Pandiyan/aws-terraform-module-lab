6daf276592432f16c98afbef2784e2ce8dfae3bb
Akash-Pandiyan <akash.ap.09@gmail.com>
2026-08-13 23:18:31 +0530

    removed az

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,6 @@ resource "aws_subnet" "mysubnets" {
   vpc_id = "vpc-0d73514c2a09be7d2"
   for_each = var.subnets
   cidr_block = each.value.cidr
-  availability_zone = each.value.az
   tags = {
     Name = each.key
   }
