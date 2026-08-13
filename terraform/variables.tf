fd262bce096c41b75c8de4c20bd2fee44ba19d17
Akash-Pandiyan <akash.ap.09@gmail.com>
2026-08-13 23:15:17 +0530

    subnets creation

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,28 @@
 variable "bucket_name" {
   default = "my-bucket-test-cicd-26899"
 }
+
+variable "subnets" {
+  type = map(object({
+    cidr = string
+    az = string
+  }))
+  default = {
+    "public-1" = {
+      cidr = "10.0.1.0/24"
+      az = "ap-south-1a"
+    }
+    "public-2" = {
+      cidr = "10.0.2.0/24"
+      az = "ap-south-1b"
+    }
+    "private-1" = {
+      cidr = "10.0.11.0/24"
+      az = "ap-south-1a"
+    }
+    "private-2" = {
+      cidr = "10.0.12.0/24"
+      az = "ap-south-1b"
+    }
+  }
+}
