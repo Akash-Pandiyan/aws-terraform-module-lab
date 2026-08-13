0879a5fb68645435c18fafe7dd19061af7dfd6ea
Akash-Pandiyan <akash.ap.09@gmail.com>
2026-08-14 00:08:45 +0530

    logic update

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,7 +37,7 @@ resource "aws_route_table" "public_rt" {
 
 
 resource "aws_route_table_association" "rt_associate" {
-  for_each = { for k, v in var.subnets : k => v if v.public }
+  for_each = { for k, v in var.subnets : k => v if k.true }
   subnet_id      = aws_subnet.mysubnets[each.key].id
   route_table_id = aws_route_table.public_rt.id
 }
